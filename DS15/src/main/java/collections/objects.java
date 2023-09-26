98ee5a933930dee7c02af80f8769704466b2760b
Daksh Sharma <95771231+Chizubaga@users.noreply.github.com>
2023-09-26 10:34:20 +0530

    Add files via upload

---
+++ DS15/src/main/java/collections/objects.java
@@ -0,0 +1,33 @@
+package collections;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+public class objects {
+    public static void main (String [] args){
+        ArrayList l1 = new ArrayList();
+        List l2 = new ArrayList();
+        Collection c1 = new ArrayList();
+        Iterable It = new ArrayList();
+
+        l1.add(10);
+        l1.add("HIHI");
+        l1.add(12.34f);
+        l1.add(12.456);
+        System.out.println(l1);
+
+        l2.add(34);
+        l2.add("Batch1");
+
+        System.out.println(l2);
+
+        c1.add(10);
+        c1.add("HIHI");
+        c1.add(12.34f);
+        c1.add(12.456);
+        System.out.println(c1);
+
+        for (Object b :c1)
+            System.out.println(b+"  =>  "+b.getClass());
+    }
+}
