6a6c6571bd87b6131e1238eb1673e2f51e2939fa
Daksh Sharma <95771231+Chizubaga@users.noreply.github.com>
2023-09-27 09:17:07 +0530

    Update ArrayList1.java

--- DS15/src/main/java/collections/ArrayList1.java
+++ DS15/src/main/java/collections/ArrayList1.java
@@ -12,13 +12,13 @@ public class ArrayList1
         l1.add("six");
 
         System.out.println("l1: "+l1);
-        System.out.println("l1.contains(\"six\" :"+l1.contains());
+        System.out.println("l1.contains(\"six\" :"+l1.contains(l1));
 
         ArrayList<String> l2 = new ArrayList<>();
         l2.add("one");
         l2.add("one");
         l2.add("one");
-        System.out.println(l1.containsAll(L2));
+        System.out.println(l1.containsAll(l2));
 
         System.out.println("l1: "+l1);
         l1.remove(1);
